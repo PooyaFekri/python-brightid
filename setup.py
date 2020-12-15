cb81918fbcf9c17c8482733a72e463637bc3de31
killing_joke <pooyafekri79@gmail.com>
2020-12-16 00:40:10 +0330

    edit setup.py change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'python-brightid',
   packages = ['brightid'],
-  version = '1',
+  version = '1.00',
   license='MIT',
   description = 'SDK for integrating with BrightId!',
   author = 'Pooya Fekri',
